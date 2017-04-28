a385727b325eca220725fc813faa4905f543fc1e
Thomas Khuu <tk7315@hotmail.com>
2017-04-27 22:02:26 -0400

    Update Recurrence2_13.java

--- AofA/Recurrence2_13.java
+++ AofA/Recurrence2_13.java
@@ -1,47 +1,65 @@
 /*
- * Name: Thomas Khuu
- * Date: 4-7-17
- * Analysis of Algorithms by Sedgewick and Flajolet
+ * Name:         Thomas Khuu
+ * Date:         4-7-17
+ * Last updated: 4-27-17
+ *
+ * An Introduction to the Analysis of Algorithms by Sedgewick and Flajolet
  * Problem 2.13
  *  -originally solved in August 2016 by iteration
  * 
- * java Recurrence2_13 65
- *  Original recurrence relation
- *  33.5
- *  ----------------------------
- *  Solution from simplified version
- *  33.5
+ * % java Recurrence2_13 65
+ *    Original recurrence relation
+ *    33.5
+ *    ----------------------------
+ *    Solution from simplified version
+ *    33.5
  *  
- * java Recurrence2_13 570
- *  Original recurrence relation
- *  286.0
- *  ----------------------------
- *  Solution from simplified version
- *  286.0
+ * % java Recurrence2_13 570
+ *    Original recurrence relation
+ *    286.0
+ *    ----------------------------
+ *    Solution from simplified version
+ *    286.0
  *
- * java Recurrence2_13 1123
- *  Original recurrence relation
- *  562.5
- *  ----------------------------
- *  Solution from simplified version
- *  562.5
+ * % java Recurrence2_13 1123
+ *    Original recurrence relation
+ *    562.5
+ *    ----------------------------
+ *    Solution from simplified version
+ *    562.5
  */
 package edu.princeton.cs.algs4;
 import edu.princeton.cs.algs4.StdOut;
 
-public class Recurrence2_13 {
-    public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        double[] A = new double[N + 1];
+public class Recurrence2_13 implements Sequence {
+    
+    private final double[] A;
+    
+    public Recurrence2_13(int maxN) {
+        A = new double[maxN + 1];
         A[0] = 1.0;
-        for (int i = 1; i <= N; i++)
-            A[i] = (i / (i + 1.0))*A[i - 1] + 1;
+        
+        for (int N = 1; N <= maxN; N++)
+            A[N] = (N/(N + 1.0))*A[N - 1] + 1;
+    }
+    
+    public double simplified(int N) {
+        return N/2.0 + 1;
+    }
+    
+    public double eval(int N) {
+        return A[N];
+    }
+    
+    public static void main(String[] args) {
+        int maxN = Integer.parseInt(args[0]);
+        Recurrence2_13 r = new Recurrence2_13(maxN);
+        
         StdOut.println("Original recurrence relation");
-        StdOut.println(A[N]);
+        StdOut.println(r.eval(maxN));
         
         StdOut.println("----------------------------");
-        double A_N = N/2.0 + 1;
         StdOut.println("Solution from simplified version");
-        StdOut.println(A_N);
+        StdOut.println(r.simplified(maxN));
     }
 }
