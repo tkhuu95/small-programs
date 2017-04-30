f2e906c1a19bb48c472cbd34c35a7b171c9f54b2
Thomas <tk7315@hotmail.com>
2017-04-30 12:21:24 -0400

    Update Sequence.java

--- AofA/Sequence.java
+++ AofA/Sequence.java
@@ -1,7 +1,7 @@
 /**
  * Name:         Thomas Khuu
  * Date:         4-25-17
- * Last updated: 4-27-17
+ * Last updated: 4-30-17
  * 
  * An interface used by Sedgewick in his book with Flajolet, An Introduction
  * to the Analysis of Algorithms except for the simplified method
@@ -10,7 +10,7 @@ public interface Sequence {
     
     // provides the solved simplified version of the recurrence relation
     // and evaluates it at specified N
-    public double simplified(int N); 
+    public abstract double simplified(int N); 
     
-    public double eval(int N);
+    public abstract double eval(int N);
 }
